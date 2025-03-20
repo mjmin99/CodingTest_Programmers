9f64869a360531acdeb9f083f0b0d4bd8c170d49
mjmin99 <mjmin99@naver.com>
2025-03-20 16:40:39 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.25 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int pizza = 0 ;
+        if(n<=7)
+        {
+            pizza = 1;
+        }
+        else if(n%7 == 0)
+        {
+            pizza = n/7 ;
+        }
+        else
+        {
+            pizza = n/7 + 1;
+        }
+        
+        int answer = pizza;
+        return answer;
+    }
+}
