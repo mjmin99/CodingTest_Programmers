d32ee7c8e18a08796160189a82782ed598fa7776
민만준 <mjmin99@naver.com>
2025-11-08 12:17:49 +0900

    [level 0] Title: 배열의 유사도, Time: 1.07 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cs
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cs
@@ -1,16 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 public class Solution {
     public int solution(string[] s1, string[] s2) {
         int answer = 0;
         
-         for (int i = 0; i < s1.Length; i++) {
-            for (int j = 0; j < s2.Length; j++) {
-                if (s1[i] == s2[j]) {
-                    answer++;
-                }
-            }
+        // 해쉬셋으로 풀어보겠음
+        //  for (int i = 0; i < s1.Length; i++) { 
+        //     for (int j = 0; j < s2.Length; j++) {
+        //         if (s1[i] == s2[j]) {
+        //             answer++;
+        //         }
+        //     }
+        // }
+        
+        HashSet<string> set = new HashSet<string>(s1);
+        int count = 0;
+        
+        foreach (string item in s2)
+        {
+            if(set.Contains(item)) count++;
         }
+        
+        
+        answer = count;
         
         return answer;
     }
