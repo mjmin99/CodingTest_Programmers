3b12958e3020576b3371d6af9c7e0d5d622bfa05
민만준 <mjmin99@naver.com>
2025-11-12 12:37:55 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.41 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int count = 0;
+        int num = 0;
+
+        while (count < n) {
+            num++;
+
+            // 3의 배수거나 숫자 3이 포함되어 있으면 건너뛰기
+            if (num % 3 == 0 || num.ToString().Contains('3')) {
+                continue;
+            }
+
+            count++;
+        }
+
+        return num;
+    }
+}
