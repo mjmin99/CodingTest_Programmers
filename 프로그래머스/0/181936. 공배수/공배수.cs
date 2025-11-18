e39f4d6210bf07ced2cd9b8849557dd1ebcb6536
민만준 <mjmin99@naver.com>
2025-11-18 09:41:56 +0900

    [level 0] Title: 공배수, Time: 0.19 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if(number % n == 0)
+        {
+            if(number % m == 0)
+            {
+                answer = 1;
+            }
+        }
+            
+        return answer;
+    }
+}
