66d7651dacf040a2f288f1c7e206662aeeeb35e1
민만준 <mjmin99@naver.com>
2025-11-19 13:31:16 +0900

    [level 0] Title: 이어 붙인 수, Time: 1.28 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        string odd = "";
+        string even = "";
+        
+        for(int i = 0; i < num_list.Length; i++)
+        {
+            if(num_list[i] % 2 == 0) // 짝수라면
+            {
+                even += num_list[i].ToString();
+            }
+            else
+            {
+                odd += num_list[i].ToString();
+            }
+        }
+        
+        answer += Int32.Parse(even);
+        answer += Int32.Parse(odd);
+        
+        return answer;
+    }
+}
