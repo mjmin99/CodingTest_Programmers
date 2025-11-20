895bd8dba8ffb4d523eb0de11caf983a91f00ec7
민만준 <mjmin99@naver.com>
2025-11-20 13:27:52 +0900

    [level 0] Title: 수 조작하기 2, Time: 4671.75 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public string solution(int[] numLog) {
+        string answer = "";
+        
+            for(int i = 1; i< numLog.Length; i++)
+            {
+                int diff = numLog[i]-numLog[i-1];
+                
+                if(diff == 1) answer += "w";
+                else if(diff == -1) answer += "s";
+                else if(diff == 10) answer += "d";
+                else if(diff == -10) answer += "a";
+            }
+        
+        return answer;
+    }
+}
