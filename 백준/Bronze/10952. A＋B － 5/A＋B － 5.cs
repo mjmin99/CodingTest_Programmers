e5001be2ba9ec22766787ed3f78e59d100146678
민만준 <mjmin99@naver.com>
2025-11-21 15:08:34 +0900

    [Bronze V] Title: A+B - 5, Time: 56 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cs
@@ -0,0 +1,20 @@
+using System;
+
+class akswns
+{
+    static void Main()
+    {
+        while(true)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            
+            if(a+b ==0)
+                break;
+            
+            Console.WriteLine(a+b);
+        }
+        
+    }
+}
