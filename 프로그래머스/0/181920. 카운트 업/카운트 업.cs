2a999df2b114152fe88efc82c323b8f3aea02e3a
민만준 <mjmin99@naver.com>
2025-11-21 13:34:54 +0900

    [level 0] Title: 카운트 업, Time: 0.20 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+        int temp = 0;
+        for(int i = start_num; i <= end_num; i++)
+        {
+            answer[temp] = i;
+            temp++;
+        }
+        return answer;
+    }
+}
