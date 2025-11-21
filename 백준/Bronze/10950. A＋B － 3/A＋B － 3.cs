170d82cd10b37ef5bef578984341ab59cd73f9df
민만준 <mjmin99@naver.com>
2025-11-21 15:03:12 +0900

    [Bronze V] Title: A+B - 3, Time: 52 ms, Memory: 5636 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cs
@@ -0,0 +1,18 @@
+using System;
+
+class akswns
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        
+        for(int i = 0; i < t; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            
+            Console.WriteLine(a+b);
+        }
+    }
+}
