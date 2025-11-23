00ee9d8d5fb53d94cfea39997116e563b891f467
민만준 <mjmin99@naver.com>
2025-11-23 12:34:16 +0900

    [level 0] Title: 세로 읽기, Time: 0.92 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int m, int c) {
+        string answer = "";
+
+        for(int i = 0; i < my_string.Length / m; i++)
+        {
+            answer += my_string[i *m + (c-1)];
+        }
+        
+        return answer;
+    }
+}
