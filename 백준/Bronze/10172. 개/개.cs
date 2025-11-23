fd12ad9636111a93caab43f4fdb368695fa73506
민만준 <mjmin99@naver.com>
2025-11-23 14:15:29 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
+    }
+}
