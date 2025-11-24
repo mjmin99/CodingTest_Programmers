122bd6101f7c79510d966c8c27c2743e2529bb3f
민만준 <mjmin99@naver.com>
2025-11-24 17:21:35 +0900

    [Bronze II] Title: 음계, Time: 44 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cs
@@ -0,0 +1,16 @@
+using System;
+
+class akswns
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        
+        if(input == "1 2 3 4 5 6 7 8")
+        {Console.WriteLine("ascending");}
+        else if(input == "8 7 6 5 4 3 2 1")
+        {Console.WriteLine("descending");}
+        else
+        {Console.WriteLine("mixed");}
+    }
+}
