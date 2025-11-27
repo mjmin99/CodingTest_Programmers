2e7fadeb5c82dd2112d878633509e6a7377e9d67
민만준 <mjmin99@naver.com>
2025-11-27 21:56:38 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 12.22 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = new int[arr.Length];
+        
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i] >= 50 && arr[i]%2 ==0)
+            {
+                arr[i] = arr[i]/2;
+            }
+            else if(arr[i] <= 50 && arr[i]%2 == 1)
+            {
+                arr[i] = arr[i]*2;
+            }
+        }
+
+        return arr;
+    }
+}
