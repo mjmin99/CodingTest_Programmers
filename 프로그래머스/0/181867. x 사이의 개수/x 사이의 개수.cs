607117d1622ea7dfd6124d7483d250101fcbf451
민만준 <mjmin99@naver.com>
2025-11-30 20:11:15 +0900

    [level 0] Title: x 사이의 개수, Time: 3.43 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int[] solution(string myString) {
+        string[] strArr = myString.Split('x');
+        int[] answer = new int[strArr.Length];
+        
+        for(int i = 0; i < strArr.Length; i++)
+        {
+            answer[i] = strArr[i].Length;
+        }
+        return answer;
+    }
+}
